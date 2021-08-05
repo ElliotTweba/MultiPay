0d14b5bb4d2c25734ccf4a57516d25d109e3b385
Elliot <tsekisoelliot1535@gmail.com>
2021-08-05 06:17:28 -0700

    Add files via upload

---
+++ components/Layout/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Navbar, Nav, NavItem } from 'reactstrap';
+
+
+const Footer = () => {
+  return (
+    <Navbar>
+      <Nav navbar>
+              <NavItem>
+                  <center>Copy write @ multipay. <br /> Design by :-- Tsekiso</center>
+        </NavItem>
+      </Nav>
+    </Navbar>
+  );
+};
+
+export default Footer;
