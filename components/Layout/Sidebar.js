a880ba7216c6f17fbd710dfa13dc0312e961f97a
Elliot <tsekisoelliot1535@gmail.com>
2021-08-05 16:21:06 -0700

    Add files via upload

--- components/Layout/Sidebar.js
+++ components/Layout/Sidebar.js
@@ -1,4 +1,4 @@
-import logo200Image from 'assets/img/logo/logo_200.png';
+
 import sidebarBgImage from 'assets/img/sidebar/sidebar-4.jpg';
 import SourceLink from 'components/SourceLink';
 import React from 'react';
@@ -81,13 +81,7 @@ class Sidebar extends React.Component {
         <div className={bem.e('content')}>
           <Navbar>
             <SourceLink className="navbar-brand d-flex">
-              <img
-                src={logo200Image}
-                width="40"
-                height="30"
-                className="pr-2"
-                alt=""
-              />
+              
               <span className="text-white">
                 MultiPay
               </span>
